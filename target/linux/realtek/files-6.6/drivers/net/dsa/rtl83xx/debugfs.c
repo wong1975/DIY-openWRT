openwrt/openwrt

realtek: add port_masks, vlan_profiles and vlans debugfs tables
As we are going to make changes to the port masks, vlan profiles and
vlans with the upcoming patches export them to debugfs for easier
debugging and monitoring.

Signed-off-by: Linus Lüssing <linus.luessing@c0d3.blue>

target/linux/realtek/files-6.6/drivers/net/dsa/rtl83xx/debugfs.c
@@ -364,6 +364,148 @@ static const struct file_operations l2_table_fops = {
 	.release = single_release,
 };
 
+static int pmsks_table_show(struct seq_file *m, void *v)
+{
+	struct rtl838x_switch_priv *priv = m->private;
+
+	mutex_lock(&priv->reg_mutex);
+
+	for (int i = 0; i < MAX_MC_PMASKS; i += 4) {
+		char o[4] = { '(', '(', '(', '(' };
+		char c[4] = { ')', ')', ')', ')' };
+
+		if (test_bit(i, priv->mc_group_bm))
+			o[0] = c[0] = ' ';
+		if (test_bit((i + 1), priv->mc_group_bm))
+			o[1] = c[1] = ' ';
+		if (test_bit((i + 2), priv->mc_group_bm))
+			o[2] = c[2] = ' ';
+		if (test_bit((i + 3), priv->mc_group_bm))
+			o[3] = c[3] = ' ';
+
+		seq_printf(m, "%04i: %c0x%016llx%c %c0x%016llx%c %c0x%016llx%c %c0x%016llx%c\n", i,
+			   o[0], priv->r->read_mcast_pmask(i), c[0],
+			   o[1], priv->r->read_mcast_pmask(i + 1), c[1],
+			   o[2], priv->r->read_mcast_pmask(i + 2), c[2],
+			   o[3], priv->r->read_mcast_pmask(i + 3), c[3]);
+	}
+
+	seq_printf(m, "MC_PMASK_ALL_PORTS (%i): 0x%016llx\n",
+		   MC_PMASK_ALL_PORTS_IDX,
+		   priv->r->read_mcast_pmask(MC_PMASK_ALL_PORTS_IDX));
+
+	mutex_unlock(&priv->reg_mutex);
+
+	return 0;
+}
+
+static int pmsks_table_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, pmsks_table_show, inode->i_private);
+}
+
+static const struct file_operations pmsks_table_fops = {
+	.owner = THIS_MODULE,
+	.open = pmsks_table_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int vlan_profiles_show(struct seq_file *m, void *v)
+{
+	struct rtl838x_switch_priv *priv = m->private;
+	struct rtl83xx_vlan_profile profile;
+	int ret, profiles_max;
+
+	if (!priv->r->vlan_profile_get)
+		return -ENOTSUPP;
+
+	profiles_max = max(RTL838X_VLAN_PROFILE_MAX, RTL839X_VLAN_PROFILE_MAX);
+	profiles_max = max(profiles_max, RTL930X_VLAN_PROFILE_MAX);
+	profiles_max = max(profiles_max, RTL931X_VLAN_PROFILE_MAX);
+
+	mutex_lock(&priv->reg_mutex);
+
+	seq_printf(m,
+		   "prof-idx: L2 learn | UNKN L2MC FLD PMSK (IDX) | UNKN IPMC FLD PMSK (IDX) | UNKN IPv6MC FLD PMSK (IDX)\n");
+	for (int i = 0; i <= profiles_max; i++) {
+		ret = priv->r->vlan_profile_get(i, &profile);
+		if (ret < 0)
+			break;
+
+		if (profile.pmsk_is_idx)
+			seq_printf(m, "%i: %i %03i %03i %03i\n", i,
+				   profile.l2_learn,
+				   profile.unkn_mc_fld.pmsks_idx.l2,
+				   profile.unkn_mc_fld.pmsks_idx.ip,
+				   profile.unkn_mc_fld.pmsks_idx.ip6);
+		else
+			seq_printf(m, "%i: %i 0x%016llx 0x%016llx 0x%016llx\n", i,
+				   profile.l2_learn,
+				   profile.unkn_mc_fld.pmsks.l2,
+				   profile.unkn_mc_fld.pmsks.ip,
+				   profile.unkn_mc_fld.pmsks.ip6);
+	}
+
+	mutex_unlock(&priv->reg_mutex);
+
+	return 0;
+}
+
+static int vlan_profiles_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, vlan_profiles_show, inode->i_private);
+}
+
+static const struct file_operations vlan_profiles_fops = {
+	.owner = THIS_MODULE,
+	.open = vlan_profiles_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int vlan_table_show(struct seq_file *m, void *v)
+{
+	struct rtl838x_switch_priv *priv = m->private;
+	struct rtl838x_vlan_info info;
+
+	if (!priv->r->vlan_tables_read)
+		return -ENOTSUPP;
+
+	mutex_lock(&priv->reg_mutex);
+
+	for (int i = 0; i < MAX_VLANS; i++) {
+		priv->r->vlan_tables_read(i, &info);
+
+		if (!info.tagged_ports)
+			continue;
+
+		seq_printf(m, "%i: %i 0x%016llx 0x%016llx\n", i,
+			   info.profile_id,
+			   info.untagged_ports,
+			   info.tagged_ports);
+	}
+
+	mutex_unlock(&priv->reg_mutex);
+
+	return 0;
+}
+
+static int vlan_table_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, vlan_table_show, inode->i_private);
+}
+
+static const struct file_operations vlan_table_fops = {
+	.owner = THIS_MODULE,
+	.open = vlan_table_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
 static ssize_t age_out_read(struct file *filp, char __user *buffer, size_t count,
 			     loff_t *ppos)
 {
@@ -696,6 +838,12 @@ void rtl838x_dbgfs_init(struct rtl838x_switch_priv *priv)
 	debugfs_create_file("drop_counters", 0400, rtl838x_dir, priv, &drop_counter_fops);
 
 	debugfs_create_file("l2_table", 0400, rtl838x_dir, priv, &l2_table_fops);
+
+	debugfs_create_file("port_masks_table", 0400, rtl838x_dir, priv, &pmsks_table_fops);
+
+	debugfs_create_file("vlan_profiles", 0400, rtl838x_dir, priv, &vlan_profiles_fops);
+
+	debugfs_create_file("vlan_table", 0400, rtl838x_dir, priv, &vlan_table_fops);
 
 	return;
 err:

wong1975/DIY-openWRT

target/linux/realtek/files-6.6/drivers/net/dsa/rtl83xx/debugfs.c
@@ -364,6 +364,148 @@ static const struct file_operations l2_table_fops = {
 	.release = single_release,
 };
 
+static int rtldsa_pmsks_table_show(struct seq_file *m, void *v)
+{
+	struct rtl838x_switch_priv *priv = m->private;
+
+	mutex_lock(&priv->reg_mutex);
+
+	for (int i = 0; i < MAX_MC_PMASKS; i += 4) {
+		char o[4] = { '(', '(', '(', '(' };
+		char c[4] = { ')', ')', ')', ')' };
+
+		if (test_bit(i, priv->mc_group_bm))
+			o[0] = c[0] = ' ';
+		if (test_bit((i + 1), priv->mc_group_bm))
+			o[1] = c[1] = ' ';
+		if (test_bit((i + 2), priv->mc_group_bm))
+			o[2] = c[2] = ' ';
+		if (test_bit((i + 3), priv->mc_group_bm))
+			o[3] = c[3] = ' ';
+
+		seq_printf(m, "%04i: %c0x%016llx%c %c0x%016llx%c %c0x%016llx%c %c0x%016llx%c\n", i,
+			   o[0], priv->r->read_mcast_pmask(i), c[0],
+			   o[1], priv->r->read_mcast_pmask(i + 1), c[1],
+			   o[2], priv->r->read_mcast_pmask(i + 2), c[2],
+			   o[3], priv->r->read_mcast_pmask(i + 3), c[3]);
+	}
+
+	seq_printf(m, "MC_PMASK_ALL_PORTS (%i): 0x%016llx\n",
+		   MC_PMASK_ALL_PORTS_IDX,
+		   priv->r->read_mcast_pmask(MC_PMASK_ALL_PORTS_IDX));
+
+	mutex_unlock(&priv->reg_mutex);
+
+	return 0;
+}
+
+static int rtldsa_pmsks_table_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, rtldsa_pmsks_table_show, inode->i_private);
+}
+
+static const struct file_operations rtldsa_pmsks_table_fops = {
+	.owner = THIS_MODULE,
+	.open = rtldsa_pmsks_table_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int rtldsa_vlan_profiles_show(struct seq_file *m, void *v)
+{
+	struct rtl838x_switch_priv *priv = m->private;
+	struct rtl83xx_vlan_profile profile;
+	int ret, profiles_max;
+
+	if (!priv->r->vlan_profile_get)
+		return -ENOTSUPP;
+
+	profiles_max = max(RTL838X_VLAN_PROFILE_MAX, RTL839X_VLAN_PROFILE_MAX);
+	profiles_max = max(profiles_max, RTL930X_VLAN_PROFILE_MAX);
+	profiles_max = max(profiles_max, RTL931X_VLAN_PROFILE_MAX);
+
+	mutex_lock(&priv->reg_mutex);
+
+	seq_printf(m,
+		   "prof-idx: L2 learn | UNKN L2MC FLD PMSK (IDX) | UNKN IPMC FLD PMSK (IDX) | UNKN IPv6MC FLD PMSK (IDX)\n");
+	for (int i = 0; i <= profiles_max; i++) {
+		ret = priv->r->vlan_profile_get(i, &profile);
+		if (ret < 0)
+			break;
+
+		if (profile.pmsk_is_idx)
+			seq_printf(m, "%i: %i %03i %03i %03i\n", i,
+				   profile.l2_learn,
+				   profile.unkn_mc_fld.pmsks_idx.l2,
+				   profile.unkn_mc_fld.pmsks_idx.ip,
+				   profile.unkn_mc_fld.pmsks_idx.ip6);
+		else
+			seq_printf(m, "%i: %i 0x%016llx 0x%016llx 0x%016llx\n", i,
+				   profile.l2_learn,
+				   profile.unkn_mc_fld.pmsks.l2,
+				   profile.unkn_mc_fld.pmsks.ip,
+				   profile.unkn_mc_fld.pmsks.ip6);
+	}
+
+	mutex_unlock(&priv->reg_mutex);
+
+	return 0;
+}
+
+static int rtldsa_vlan_profiles_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, rtldsa_vlan_profiles_show, inode->i_private);
+}
+
+static const struct file_operations rtldsa_vlan_profiles_fops = {
+	.owner = THIS_MODULE,
+	.open = rtldsa_vlan_profiles_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int rtldsa_vlan_table_show(struct seq_file *m, void *v)
+{
+	struct rtl838x_switch_priv *priv = m->private;
+	struct rtl838x_vlan_info info;
+
+	if (!priv->r->vlan_tables_read)
+		return -ENOTSUPP;
+
+	mutex_lock(&priv->reg_mutex);
+
+	for (int i = 0; i < MAX_VLANS; i++) {
+		priv->r->vlan_tables_read(i, &info);
+
+		if (!info.tagged_ports)
+			continue;
+
+		seq_printf(m, "%i: %i 0x%016llx 0x%016llx\n", i,
+			   info.profile_id,
+			   info.untagged_ports,
+			   info.tagged_ports);
+	}
+
+	mutex_unlock(&priv->reg_mutex);
+
+	return 0;
+}
+
+static int rtldsa_vlan_table_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, rtldsa_vlan_table_show, inode->i_private);
+}
+
+static const struct file_operations rtldsa_vlan_table_fops = {
+	.owner = THIS_MODULE,
+	.open = rtldsa_vlan_table_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
 static ssize_t age_out_read(struct file *filp, char __user *buffer, size_t count,
 			     loff_t *ppos)
 {
@@ -696,6 +838,15 @@ void rtl838x_dbgfs_init(struct rtl838x_switch_priv *priv)
 	debugfs_create_file("drop_counters", 0400, rtl838x_dir, priv, &drop_counter_fops);
 
 	debugfs_create_file("l2_table", 0400, rtl838x_dir, priv, &l2_table_fops);
+
+	debugfs_create_file("port_masks_table", 0400, rtl838x_dir, priv,
+			    &rtldsa_pmsks_table_fops);
+
+	debugfs_create_file("vlan_profiles", 0400, rtl838x_dir, priv,
+			    &rtldsa_vlan_profiles_fops);
+
+	debugfs_create_file("vlan_table", 0400, rtl838x_dir, priv,
+			    &rtldsa_vlan_table_fops);
 
 	return;
 err: